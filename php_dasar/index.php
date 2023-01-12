e4c152893bbf9ebc2a1133a42851cf049535f490
bagusuntoro <untorobagus65@gmail.com>
2023-01-12 08:17:32 +0700

    fixed task 1

--- php_dasar/index.php
+++ php_dasar/index.php
@@ -1,18 +1,19 @@
 <?php
-for ($i = 1; $i <= 100; $i++) {
-  if ($i % 3 == 0) { //parent condition
+for ($i = 1; $i <= 100; $i++) { // loop i = 1 to i = 100
 
-    if($i % 5 == 0){ //child condition
-      echo luas_persegi($i/3,$i/5);
-    }else{
-            echo luas_lingkaran($i/3);
-    } //akhir child condition
+  if ($i % 3 == 0) { // for check i mod 3 == 0 or not 
+
+    echo ($i % 5 == 0) ? luas_lingkaran($i/3,$i/5) : luas_lingkaran($i/3); // ternary for check (i mod 3 == 0) && i mod 5 == 0 or not
   
-  } elseif ($i % 5 == 0) {
+  } elseif ($i % 5 == 0) { // for check i mod 5 == 0 or not 
+
       echo keliling_lingkaran($i/5);
-  } else {
+
+  } else { // default i
+
     echo $i.'.00';
-  } //akhir parent condition
+
+  }
     echo '<br/>';
 }
 
@@ -20,10 +21,10 @@ for ($i = 1; $i <= 100; $i++) {
   function luas_lingkaran($angka){ //function luas lingkaran
       return round(3.14 * pow($angka, 2),2);
   }
-  function keliling_lingkaran($angka){ //function keliling
+  function keliling_lingkaran($angka){ //function keliling lingkaran
       return round(2*3.14*$angka,2);
   }
-  function luas_persegi($panjang,$lebar){ //function persegi
-      return round(($panjang/3)*($lebar/5),2);
+  function luas_persegi($panjang,$lebar){ //function luas persegi
+      return round($panjang*$lebar,2);
   }
 ?>
